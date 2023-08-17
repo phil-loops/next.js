0f11d446c43523961bd32413730360d2f8a21f1d
Jiachi Liu <inbox@huozhi.im>
2023-08-18 00:14:42 +0200

    not found default

--- packages/next/src/server/lib/router-utils/setup-dev.ts
+++ packages/next/src/server/lib/router-utils/setup-dev.ts
@@ -812,7 +812,7 @@ async function startWatcher(opts: SetupOpts) {
       let envChange = false
       let tsconfigChange = false
       let conflictingPageChange = 0
-      let hasRootAppNotFound = true // false
+      let hasRootAppNotFound = false
 
       const { appFiles, pageFiles } = opts.fsChecker
 
